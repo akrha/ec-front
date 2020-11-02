2a03f3d1fd3849e18369f578c389185d86290a73
Akira <hase.a1995@gmail.com>
2020-11-03 00:05:14 +0900

    CSS対応など

--- resources/views/common/searchform.blade.php
+++ resources/views/common/searchform.blade.php
@@ -2,13 +2,15 @@
 $q = !empty($request['q']) ? $request['q'] : '';
 $sort = !empty($request['sort']) ? $request['sort'] : '';;
 ?>
-{!! Form::open(['route' => 'items.search', 'method' => 'get']) !!}
-    {!! Form::text('q', $q) !!}
-    {!! Form::select('sort', [
-        'updatedAtDesc' => '新しい順',
-        'updatedAtAsc' => '古い順',
-        'priceAsc' => '価格が安い順',
-        'priceDesc' => '価格が高い順'
-    ], $sort) !!}
-    {!! Form::submit('検索') !!}
-{!! Form::close() !!}
+<div class="form-inline my-2 my-lg-0">
+<form method="GET" action="{{ route('items.search') }}" accept-charset="UTF-8">
+    <input class="form-control mr-sm-2" name="q" type="search" value="{{ $q }}">
+        <select name="sort" class="custom-select custom-select-lg mb-3">
+    <option value="updatedAtDesc" selected="selected">新しい順</option>
+            <option value="updatedAtAsc">古い順</option>
+            <option value="priceAsc">価格が安い順</option>
+            <option value="priceDesc">価格が高い順</option>
+        </select>
+    <input class="btn btn-outline-success my-2 my-sm-0" type="submit" value="検索">
+</form>
+</div>
